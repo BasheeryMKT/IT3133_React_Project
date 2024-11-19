ce8f684375654dd6f6f0a45ce60fa9825c6b4106
BasheeryMKT <154669750+BasheeryMKT@users.noreply.github.com>
2024-11-19 18:42:56 +0000

    table created

--- mystudent/src/App.js
+++ mystudent/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-import StudentTable from './Components/StudentTable';
-import { students } from './data/StudentsDb';
+import "./App.css";
+import TableCreate from "./Components/table";
+import "./assets/css/compo.css";
+import { students } from "./data/studentsDb.js";
+import { useState } from "react";
 
 function App() {
+  const [fontSize, setFontSize] = useState("16px");
+  const changeFontSize = (size) => {
+    switch (size) {
+      case "small":
+        setFontSize("14px");
+        break;
+      case "medium":
+        setFontSize("20px");
+        break;
+      case "large":
+        setFontSize("24px");
+        break;
+      default:
+        setFontSize("14px");
+    }
+  };
+ 
   return (
-    <div className="App">
-     <h1>Student Information Portal</h1>
-     <table>
-      <tBody>
-        <th>
-          <tr>
-            <td>
-              <StudentTable students = {students}/>
-            </td>
-          </tr>
-        </th>
-      </tBody>
-     </table>
-        
+    <>
+    <div style={{ fontSize }}>
+      <div>
+        <h3>Font Size</h3>
+        <button onClick={() => changeFontSize("small")}>S</button>
+        <button onClick={() => changeFontSize("medium")}>M</button>
+        <button onClick={() => changeFontSize("large")}>L</button>
+      </div>
+      <tr></tr>
+      <div>
+        <TableCreate students={students}></TableCreate>
+      </div>
+      <div></div>
     </div>
+  </>
   );
 }
 
